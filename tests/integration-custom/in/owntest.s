2ab9568cb2fe86f8cdc0fa0f66daad3ec19ee829
RehtiW <Etranx@outlook.com>
2024-12-27 10:24:53 +0800

    complete Itype inst

--- tests/integration-custom/in/owntest.s
+++ tests/integration-custom/in/owntest.s
@@ -1,9 +1,4 @@
 .text
-    addi t0,x0,1
-    addi t0,t0,1
-    addi t0,t0,1
-    addi t0,x0,2
-    addi t0,t0,3
-    addi t0,t0,4
-    andi t1, t0, 1  # t1 = 1
-    slli t1, t1, 2  # t1 = 4
+    addi t0,x0,-6
+    srli t0,t0,2
+    slti t0, t0, 2
