cce6fec54d96116eff33fc6bfefb30a0b598d86c
RehtiW <Etranx@outlook.com>
2024-12-29 20:41:12 +0800

    complete RType inst

--- tests/integration-custom/in/owntest.s
+++ tests/integration-custom/in/owntest.s
@@ -1,4 +1,20 @@
 .text
     addi t0,x0,-6
-    srli t0,t0,2
-    slti t0, t0, 2
+    srai t0,t0,2 # t0 = -2
+    addi t1,x0,2 # t1 = 2
+    sub t1,t1,t0 # t1 = 4
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sub t1,t1,t0
+    sra t1, t1, t1     
+    mul t1,t1,t1
+    mulh t1,t1,t1
+    mulhu t1,t1,t1
+    
+    
